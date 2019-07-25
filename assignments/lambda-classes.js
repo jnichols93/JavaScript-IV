c1468f2dbc908a74399a1a78540d8191b1d39694
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-07-25 15:39:37 -0500

    Update lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,37 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(atts);
+    this.name= atts.name;
+    this.age= atts.age;
+    this.location = atts.location;
+
+    speak(){
+        console.log(`hello my name is ${this.name} I am from ${this.location}`)
+    }
+}
+
+class Instructor extends Person{
+    constructor(insAtts){
+        super(insAtts);
+        this.specialty = insAtts.specialty;
+        this.favLanguage = insAtts.favLanguage;
+        this.catchphrase = insAtts.catchphrase;
+    }
+    demo(){
+
+    }
+
+    grade(){
+
+    }
+}
+
+class Student extends Instructor{
+    constructor(stuAtss){
+        super(stuAtss);
+        this.previousBackground = stuAtss.previousBackground;
+        this.className=stuAtss.className;
+        this.favSubjects=stuAtss.favSubjects;
+    }
+    
+}
