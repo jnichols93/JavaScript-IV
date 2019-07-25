976c04be6ac341409b423525a55a4595358459cb
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-07-25 16:21:02 -0500

    Update lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,13 +1,14 @@
 // CODE here for your Lambda Classes
 class Person{
-    constructor(atts);
+    constructor(atts){
     this.name= atts.name;
     this.age= atts.age;
     this.location = atts.location;
-
+    }
     speak(){
         console.log(`hello my name is ${this.name} I am from ${this.location}`)
-    }
+    };
+
 }
 
 class Instructor extends Person{
@@ -17,21 +18,84 @@ class Instructor extends Person{
         this.favLanguage = insAtts.favLanguage;
         this.catchphrase = insAtts.catchphrase;
     }
-    demo(){
-
-    }
+    demo(subject){
+            console.log(`Today we are learning about ${subject}`)
+    };
 
     grade(){
+        console.log(`${Student} Recieves a perfect grade on ${subject}`)
 
-    }
+    };
 }
 
-class Student extends Instructor{
+class Student extends Person{
     constructor(stuAtss){
         super(stuAtss);
         this.previousBackground = stuAtss.previousBackground;
         this.className=stuAtss.className;
         this.favSubjects=stuAtss.favSubjects;
     }
+    listSubjects(){
+        console.log(`${this.favSubjects}`)
+    };
+    PRAssignment(){
+
+    };
+
+    sprintChallenge(){
+
+    };
     
 }
+
+class ProjectManager extends Instructor{
+    cunstructor(pmstats){
+        super(pmstats);
+        this.gradClassName = pmstats.gradClassName;
+        this.favInstructor = pmstats.favInstructor;
+    }
+    standUp(){
+
+    };
+    debugsCode(){
+
+    };
+}
+///////////////////End Class////////////////
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+  const justin = new Student({
+      name: 'Justin',
+      location: 'Minneapolis',
+      age:26,
+      previousBackground: 'Sales Manager',
+      className: 'Web22',
+      favSubjects: ['HTML','CSS','JavaScript']
+  });
+  const coco = new Student({
+    name: 'Coco',
+    location:'Texas',
+    age:6,
+    previousBackground: 'Dog',
+    className:'drop out',
+    favSubjects:['Tail Chasing', 'Going on Walks','Eating']
+
+  });
+
+  const jim = new ProjectManager({
+    name: 'Jim',
+    location: 'Rhode Island',
+    age: 77,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `forget the homies`,
+    gradClassName:'Web 11',
+    favInstructor: 'Britt'
+  });
+  /////End objects
